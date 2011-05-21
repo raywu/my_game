baba4d42827a1d761b1cc6c3da203f88fa2c4c5b
Ray Wu <raymond.cj.wu@gmail.com>
2011-05-20 23:56:52 -0400

    Finished with Invader movements

---
+++ my_game.rb
@@ -0,0 +1,37 @@
+$: << File.expand_path(File.dirname(__FILE__)) 
+require 'rubygems'
+require 'gosu'
+require 'player'
+
+class MyGame < Gosu::Window
+  def initialize
+    super(300, 400, false)
+    @player1 = Player.new(self)
+  end
+  
+  def update
+    if button_down? Gosu::Button::KbLeft
+      @player1.move_left
+    end
+    
+    if button_down? Gosu::Button::KbUp
+      @player1.move_up
+    end
+    
+    if button_down? Gosu::Button::KbDown
+      @player1.move_down
+    end
+    
+    if button_down? Gosu::Button::KbRight
+      @player1.move_right
+    end
+  end
+  
+  def draw
+    @player1.draw
+  end
+
+end
+
+window = MyGame.new
+window.show
---
+++ player.rb
@@ -0,0 +1,46 @@
+class Player
+  
+  def initialize(game_window)
+    @game_window = game_window
+    @icon = Gosu::Image.new(@game_window, "images/player2_icon.png", true)
+    @x = 50
+    @y = 50
+  end
+  
+  def draw
+    @icon.draw(@x, @y, 1)
+  end
+  
+  def move_left
+    if @x < 0
+      @x = 0
+    else
+      @x = @x - 10
+    end
+  end
+  
+  def move_right
+    if @x > (@game_window.width - 48)
+      @x = @game_window.width - 48
+    else
+      @x = @x + 10
+    end
+  end
+  
+  def move_up
+    if @y < 0
+      @y = 0
+    else
+      @y = @y - 10
+    end
+  end
+  
+  def move_down
+    if @y > (@game_window.height - 48)
+      @y = @game_window.height - 48
+    else
+      @y = @y + 10
+    end
+  end
+  
+end
